5609df5ce18823b52df591bb6840d34f62b8e783
Moshe <Moshe@DESKTOP-I8BM850>
2021-01-02 13:27:21 +0200

    new v1.0

---
+++ ExceptionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helper.Microservice
+{
+    public class ExceptionHelper
+    {
+        public static string GetMessages(Exception Exception, string Message = "")
+        {
+            if (Exception == null) return Message;
+
+            var message = string.IsNullOrEmpty(Message) ? Exception.Message
+                                                        : $"{Message}\\r\\n {Exception.Message}";
+            
+            return GetMessages(Exception.InnerException, message);
+        }
+    }
+}
